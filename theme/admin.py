ffba934d8c25b7dcb6090721aab45e5c8b3d3fd1
Hamza Pervez <hamza.pervez@arbisoft.com>
2017-10-06 12:38:51 +0500

    created new Speaker content type

--- theme/admin.py
+++ theme/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from mezzanine.core.admin import DisplayableAdmin
+from theme.models import Speaker
 
-# Register your models here.
+
+class SpeakerAdmin(DisplayableAdmin):
+    fieldsets = ((None, {'fields': ('title', 'designation', 'organization', 'bio', 'profile_image', 'facebook_link',
+                                    'twitter_link', 'linked_in_link', 'dribble_link')}),)
+    list_display = ('title', 'status', 'designation', 'organization')
+
+admin.site.register(Speaker, SpeakerAdmin)
--- theme/models.py
+++ theme/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from mezzanine.core.fields import FileField
+from mezzanine.pages.models import Displayable
+from mezzanine.utils.models import upload_to
 
-# Create your models here.
+
+class Speaker(Displayable):
+    designation = models.CharField(max_length=20)
+    organization = models.CharField(max_length=30)
+    bio = models.TextField()
+    profile_image = FileField(verbose_name=_("Profile Image"),
+                              upload_to=upload_to("", "teacher"),
+                              format="Image", max_length=255, null=True, blank=True)
+    facebook_link = models.URLField(blank=True, null=True)
+    twitter_link = models.URLField(blank=True, null=True)
+    linked_in_link = models.URLField(blank=True, null=True)
+    dribble_link = models.URLField(blank=True, null=True)
+
+    def get_absolute_url(self):
+        return '/speakers/'
