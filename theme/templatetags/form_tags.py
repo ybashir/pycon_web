ae30d5b0a8d29cd5b2aa54f9e26dd294706c4d36
Hamza Pervez <hamza.pervez@arbisoft.com>
2017-10-10 15:05:56 +0500

    Added form for booking tickets

---
+++ theme/templatetags/form_tags.py
@@ -0,0 +1,13 @@
+from django.template import Library
+from django.template.loader import get_template
+
+register = Library()
+
+
+@register.simple_tag(takes_context=True)
+def render_field(context, field, template="generic/form_field.html"):
+    """
+    Renders fields for a form with an optional template choice.
+    """
+    context["field"] = field
+    return get_template(template).render(context)
