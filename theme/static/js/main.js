7846cb2f1926387cefd3da78d788392e18bc68c9
murad <heyub1gm0uth>
2018-09-24 19:45:27 +0500

    Date changed for 2018 pycon event event

--- theme/static/js/main.js
+++ theme/static/js/main.js
@@ -13,7 +13,7 @@
 /* ==========================================================================
    countdown timer
    ========================================================================== */
- jQuery('#clock').countdown('2018/10/27',function(event){
+ jQuery('#clock').countdown('2018/11/17',function(event){
   var $this=jQuery(this).html(event.strftime(''
   +'<div class="time-entry days"><span>%-D</span> Days</div> '
   +'<div class="time-entry hours"><span>%H</span> Hours</div> '
