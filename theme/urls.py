a648da20890a47abb47af3f52c491f308ef88fda
Murad Habib <murad.habib@arbisoft.com>
2018-10-04 13:07:27 +0500

    Revert "- removed duplications of speakers models and templates (#99)" (#102)
    This reverts commit 7fb18ecdf127030198dc4464718a581818e7422f.

--- theme/urls.py
+++ theme/urls.py
@@ -4,7 +4,6 @@ from theme import views
 
 
 urlpatterns = [
-    url(r'^speakers/(?P<year>[0-9]{4})/$', views.SpeakersIndexView.as_view(), name='speakers_index'),
     url(r'^gallery/$', views.GalleryIndexView.as_view(), name='gallery_index'),
     url(r'^gallery/(?P<slug>[\w-]+)/$', views.GalleryDetailView.as_view(), name='gallery_detail'),
 ]
