64560d3fcc14658a2b827f9c4004ba76f2d50564
Owais <soac@outlook.com>
2019-09-11 02:07:22 +0500

    Added sponsor type support

--- theme/templatetags/sponsor_tags.py
+++ theme/templatetags/sponsor_tags.py
@@ -4,12 +4,22 @@ from django import template
 
 register = template.Library()
 
+def preprocess_sponsors(sponsors):
+    sponsors_dict = {}
+    for sponsor in sponsors:
+        key = sponsor.sponsor_type.title if sponsor.sponsor_type else ""
+        
+        if sponsors_dict.get(key) == None:
+            sponsors_dict[key] = [sponsor]
+        else:
+            sponsors_dict[key].append(sponsor)
+
+    return sponsors_dict
 
 @register.simple_tag
 def featured_sponsors(limit=4):
-    return list(Sponsor.objects.filter(featured=True)[:limit])
-
+    return preprocess_sponsors(list(Sponsor.objects.filter(featured=True).order_by('sponsor_type__order', 'title')[:limit]))
 
 @register.simple_tag
 def all_sponsors():
-    return list(Sponsor.objects.all())
+    return preprocess_sponsors(list(Sponsor.objects.all()))
